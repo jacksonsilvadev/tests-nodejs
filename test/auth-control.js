a55508f0c520f3beea123ef70cb2ee5108cd23cf
Jackson Silva <jackson.rodrigues9@hotmail.com>
2020-06-30 20:05:05 -0300

    Assync tests in auth controller

--- test/auth-control.js
+++ test/auth-control.js
@@ -5,9 +5,23 @@ const User = require('../models/user')
 const authController = require('../controllers/auth')
 
 describe('Auth Controller - Login', () => {
-  it('should throw an error with code 500 if accessing the database fails', () => {
+  it('should throw an error with code 500 if accessing the database fails', (done) => {
     sinon.stub(User, 'findOne')
     User.findOne.throws();
+
+    const req = {
+      body: {
+        email: 'test@test.com',
+        password: 'tester'
+      }
+    }
+
+    authController.login(req, {}, () => {}).then(result => {
+      expect(result).to.be.an('error')
+      expect(result).to.be.property('statusCode', 500)
+      // added done for assync functions
+      done();
+    })
 
     User.findOne.restore()
   })
