2b2453db2b0c314d98550a63ae5cbfadd8412238
Jackson Silva <jackson.rodrigues9@hotmail.com>
2020-06-29 22:15:53 -0300

    make test for jwt tokens

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,6 @@
 const {expect} = require('chai')
+const jwt = require('jsonwebtoken')
+const sinon = require('sinon')
 const authMiddleware = require('../middleware/is-auth')
 
 describe('Auth middleware', () => {
@@ -21,4 +23,37 @@ describe('Auth middleware', () => {
   
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
   })
+
+  it('should yield a userId after decoding the token', () => {
+    const req = {
+      get: (headerName) => {
+        return 'Bearer sdfafasdfsadfa'
+      }
+    }
+    // save jwt function to return value
+    sinon.stub(jwt, 'verify');
+    // Return value for test
+    jwt.verify.returns({ userId: 1 });
+    // Verify in middleware
+    authMiddleware(req, {}, () => {})
+
+    expect(req).to.have.property('userId')
+    expect(req).to.have.property('userId', 1)
+    expect(jwt.verify.called).to.be.true;
+
+    // After test restore verify native function to others tests after this
+    jwt.verify.restore();
+  })
+
+  it('should throw an error if the token cannot be verified', () => {
+    const req = {
+      get: (headerName) => {
+        return 'Bearer xyz'
+      }
+    }
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw()
+  })
+
+  
 })
