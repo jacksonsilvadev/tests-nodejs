eab4ad7cfca39530565565b89f6a34de263706d0
Jackson Silva <jackson.rodrigues9@hotmail.com>
2020-06-30 20:34:07 -0300

    Initializing test with database

--- test/auth-control.js
+++ test/auth-control.js
@@ -25,4 +25,43 @@ describe('Auth Controller - Login', () => {
 
     User.findOne.restore()
   })
+
+  it('should send a response with a valid user status for an existing user', (done) => {
+    mongoose
+      .connect(
+        'mongodb+srv://localhost:27000/test-messages?retryWrites=true'
+      )
+      .then(result => {
+        const user = new User({
+          email: 'test@test.com',
+          name: 'Test',
+          password: 'tester',
+          posts: [],
+          _id: '5c0f66b979af55031b34728a'
+        })
+        return user.save();
+      })
+      .then(() => {
+        const req = { userId: '5c0f66b979af55031b34728a' }
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: (code) => {
+            this.statusCode = code;
+            return this;
+          },
+          json: () => {
+            this.userStatus = data.status
+          }
+        };
+    
+        authController.getUserStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal('I am new!')
+          done();
+        })
+      })
+      .catch(err => console.log(err));
+
+  })
 })
